653b2c4599178372af09bc638fd4389c77680974
Thomas Fehér <thomasfeher@web.de>
2015-09-20 14:46:10 +0200

    version 4.600.2

--- README.txt
+++ README.txt
@@ -187,7 +187,7 @@ In general, programs which use Armadillo are compiled along these lines:
 If you want to use Armadillo without installation,
 or you're getting linking errors, compile along these lines:
   
-  g++ example1.cpp -o example1 -O2 -I /home/blah/armadillo-4.600.1/include -DARMA_DONT_USE_WRAPPER -lblas -llapack
+  g++ example1.cpp -o example1 -O2 -I /home/blah/armadillo-4.600.2/include -DARMA_DONT_USE_WRAPPER -lblas -llapack
   
 The above command line assumes that you have unpacked the armadillo archive into /home/blah/
 You will need to adjust this for later versions of Armadillo,
@@ -195,8 +195,8 @@ and/or if you have unpacked into a different directory.
 
 Notes:
 
-* To use the high speed OpenBLAS library instead of BLAS and LAPACK,
-  replace -lblas -llapack with -lopenblas
+* To use the high speed OpenBLAS library instead of BLAS,
+  replace -lblas -llapack with -lopenblas -llapack
   To get OpenBLAS, see http://xianyi.github.com/OpenBLAS/
   
 * On most Linux-based systems, using -lblas -llapack should be enough;
--- cmake_aux/Modules/ARMA_FindOpenBLAS.cmake
+++ cmake_aux/Modules/ARMA_FindOpenBLAS.cmake
@@ -1,37 +1,50 @@
-# - Find the OpenBLAS library (no includes)
-# This module defines
-#  OpenBLAS_LIBRARIES, the libraries needed to use OpenBLAS.
-#  OpenBLAS_FOUND, If false, do not try to use OpenBLAS.
-# also defined, but not for general use are
-#  OpenBLAS_LIBRARY, where to find the OpenBLAS library.
+set(OpenBLAS_NAMES)
+set(OpenBLAS_NAMES ${OpenBLAS_NAMES} openblaso)
+set(OpenBLAS_NAMES ${OpenBLAS_NAMES} openblasp)
+set(OpenBLAS_NAMES ${OpenBLAS_NAMES} openblas )
 
-SET(OpenBLAS_NAMES ${OpenBLAS_NAMES} openblas)
-FIND_LIBRARY(OpenBLAS_LIBRARY
-  NAMES ${OpenBLAS_NAMES}
-  PATHS /lib64 /lib /usr/lib64 /usr/lib /usr/local/lib64 /usr/local/lib
-  )
-
-IF (OpenBLAS_LIBRARY)
-  SET(OpenBLAS_LIBRARIES ${OpenBLAS_LIBRARY})
-  SET(OpenBLAS_FOUND "YES")
-ELSE (OpenBLAS_LIBRARY)
-  SET(OpenBLAS_FOUND "NO")
-ENDIF (OpenBLAS_LIBRARY)
+set(OpenBLAS_TMP_LIBRARY)
+set(OpenBLAS_TMP_LIBRARIES)
 
 
-IF (OpenBLAS_FOUND)
-   IF (NOT OpenBLAS_FIND_QUIETLY)
-      MESSAGE(STATUS "Found OpenBLAS: ${OpenBLAS_LIBRARIES}")
-   ENDIF (NOT OpenBLAS_FIND_QUIETLY)
-ELSE (OpenBLAS_FOUND)
-   IF (OpenBLAS_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find OpenBLAS")
-   ENDIF (OpenBLAS_FIND_REQUIRED)
-ENDIF (OpenBLAS_FOUND)
+foreach (OpenBLAS_NAME ${OpenBLAS_NAMES})
+  find_library(${OpenBLAS_NAME}_LIBRARY
+    NAMES ${OpenBLAS_NAME}
+    PATHS ${CMAKE_SYSTEM_LIBRARY_PATH} /lib64 /lib /usr/lib64 /usr/lib /usr/local/lib64 /usr/local/lib
+    )
+  
+  set(OpenBLAS_TMP_LIBRARY ${${OpenBLAS_NAME}_LIBRARY})
+  
+  if(OpenBLAS_TMP_LIBRARY)
+    set(OpenBLAS_TMP_LIBRARIES ${OpenBLAS_TMP_LIBRARIES} ${OpenBLAS_TMP_LIBRARY})
+  endif()
+endforeach()
 
-# Deprecated declarations.
-GET_FILENAME_COMPONENT (NATIVE_OpenBLAS_LIB_PATH ${OpenBLAS_LIBRARY} PATH)
 
-MARK_AS_ADVANCED(
-  OpenBLAS_LIBRARY
-  )
+# use only one library
+
+if(OpenBLAS_TMP_LIBRARIES)
+  list(GET OpenBLAS_TMP_LIBRARIES 0 OpenBLAS_LIBRARY)
+endif()
+
+
+if(OpenBLAS_LIBRARY)
+  set(OpenBLAS_LIBRARIES ${OpenBLAS_LIBRARY})
+  set(OpenBLAS_FOUND "YES")
+else()
+  set(OpenBLAS_FOUND "NO")
+endif()
+
+
+if(OpenBLAS_FOUND)
+  if (NOT OpenBLAS_FIND_QUIETLY)
+    message(STATUS "Found OpenBLAS: ${OpenBLAS_LIBRARIES}")
+  endif()
+else()
+  if(OpenBLAS_FIND_REQUIRED)
+    message(FATAL_ERROR "Could not find OpenBLAS")
+  endif()
+endif()
+
+
+# mark_as_advanced(OpenBLAS_LIBRARY)
