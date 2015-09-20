cbea4ad958f270268e6775a709e704cec57ef223
Thomas Fehér <thomasfeher@web.de>
2015-09-20 14:46:06 +0200

    version 4.550.0

--- include/armadillo_bits/config.hpp
+++ include/armadillo_bits/config.hpp
@@ -31,7 +31,7 @@
 
 // #define ARMA_USE_WRAPPER
 //// Comment out the above line if you're getting linking errors when compiling your programs,
-//// or if you prefer to directly link with LAPACK, BLAS or ARPACK.
+//// or if you prefer to directly link with LAPACK, BLAS or ARPACK instead of the Armadillo runtime library.
 //// You will then need to link your programs directly with -llapack -lblas instead of -larmadillo
 
 // #define ARMA_BLAS_CAPITALS
